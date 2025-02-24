143c9f15cb3fcb57830a73fd3f0ed2f8b5b0fc1d
aboodnazli <aboodnazli@gmail.com>
2025-02-24 11:13:48 +0200

    Updated progress

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -27,3 +29,20 @@ Route::post('/about', function () {
     return view('about' ,compact('name','departments'));
 
 });
+
+
+Route::get('tasks', [TaskController::class, 'index']);
+
+Route::post('create', [TaskController::class, 'create']);
+
+Route::post('delete/{id}', [TaskController::class, 'destroy']);
+
+Route::post('edit/{id}', [TaskController::class, 'edit']);
+
+Route::post('update', [TaskController::class, 'update']);
+
+Route::get('app', function () {
+    return view('layouts.app');
+});
+
+
